05f73daaf61bb791ad47f432e02c5d189505c923
Rodrigue de Guerre <99824035+Dequavious6@users.noreply.github.com>
2023-01-08 15:46:49 +0100

    Update gtk-install.sh

--- Shell/gtk-install.sh
+++ Shell/gtk-install.sh
@@ -3,7 +3,7 @@
 
 # This Software is copyright DESIGNØ®, all rights reserved.
 # Copyright © 2022 DESIGNØ SASU.
-<<LICENSE
+<<'LICENSE'
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
