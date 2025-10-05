937d0d946b22e6db1efeb107b20af0145ca0cda4
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-10-05 16:20:38 +0200

    Update embedded-swift.sh

--- src/embedded/embedded-swift.sh
+++ src/embedded/embedded-swift.sh
@@ -1,21 +1,34 @@
 #!bin/bash
 
-# Locate the toolchain
+# If you haven't already, install Homebrew
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Then Start by downloading Swiftly to Install and manage your Swift
+$ brew install swiftly
+
+# Now run swiftly init to finish the installation
+$ swiftly init
+
+# You can then install the latest (stable) version of the toolchain
+$ swiftly install latest
+
+# And check the version (optional)
+$ swift --version
+
+# Then you should locate the toolchain
 $ ls ~/Library/Developer/Toolchains/
+# swift-6.2-RELEASE.xctoolchain swift-latest.xctoolchain ... <Any other toolchain you have installed>
 
 # Find the toolchain identifier
 $ plutil -extract CFBundleIdentifier raw \
   -o - \
-  ~/Library/Developer/Toolchains/swift-DEVELOPMENT-SNAPSHOT-2024-06-13-a.xctoolchain/Info.plist
-# org.swift.59202406131a
+  ~/Library/Developer/Toolchains/swift-latest.xctoolchain/Info.plist
+# org.swift.6200202509111a
 
 # Test the toolchain
-$ TOOLCHAINS=org.swift.59202406131a swift --version
+$ TOOLCHAINS=org.swift.6200202509111a swift --version
 # Apple Swift version 5.10 (swiftlang-5.10.0.13 clang-1500.3.9.4)
 # Target: x86_64-apple-darwin23.4.0
-
-# Install Homebrew
-$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install CMake, Ninja, and dfu-util
 $ brew install cmake ninja dfu-util
@@ -49,12 +62,21 @@ $ git clone \
 $ cd ~/esp/esp-matter
 $ ./install.sh
 
-
+###
+# ! The following should be done in the shell where you plan to build your project !
+# It has to be redone every time you open a new shell.
+##
 # Setup the Swift nightly toolchain
-$ export TOOLCHAINS=org.swift.59202406131a
+$ export TOOLCHAINS=org.swift.6200202509111a
 
 # Setup the ESP-IDF environment
 $ . ~/esp/esp-idf/export.sh
 
 # Setup the ESP-Matter environment
 $ . ~/esp/esp-matter/export.sh
+
+# You can then test your installation by runing:
+$ idf.py --list-targets
+
+# And prevent potential bugs from arising during build time by runing:
+$ idf.py add-dependency "espressif/esp_diag_data_store==1.0.1"
